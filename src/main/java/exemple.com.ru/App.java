6b44fabb79787a17915399b0ad7c6e68b1736ca8
Max Prigonyuk <kael.smr@gmail.com>
2024-06-03 19:26:58 +0400

    2 hours

---
+++ src/main/java/exemple.com.ru/App.java
@@ -0,0 +1,11 @@
+package exemlpe.com.ru;
+
+
+public class App {
+    public static void main(String[] args) {
+        print();
+    }
+}
+public static void print(){
+    System.out.println("Magazine is open on 2 hours");
+}
